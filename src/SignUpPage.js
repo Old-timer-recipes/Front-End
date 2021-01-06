9d833407208b527d63ba521c8f30a4a642785b45
Monica Zwissler <mzwissler@Monicas-MacBook-Air.local>
2021-01-06 09:28:38 -0800

    form added, axios call complete.

--- src/SignUpPage.js
+++ src/SignUpPage.js
@@ -1,9 +1,74 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+
+
+
+   
+   
+
+    //////////////CODE BEGINS HERE////////////////
+const initialSUValues = {
+    name: "",
+    email: "",
+    password: "",
+    passwordConfirm: "",
+}
+
+const initialSUErrors = {
+    name: "",
+    email: "",
+    password: "",
+    passwordConfirm: "",
+}
+
+const initialSU = [];
+const initialDisabled = true;
+
 
 export default function SignUpPage() {
-    return (
+const[signup, setSignup] = useState(initialSU)
+const [signupValues, setSignupValues] = useState(initialSUValues)
+const [signupErrors, setSignupErrors] = useState(initialSUErrors)
+const [disabled, setDisabled] = useState(initialDisabled)
+
+
+const getSignup = () => {
+    axios
+        .get("https://reqres.in/api/users")
+        .then((res) => {
+            setSignup(res.data.data);
+        })
+        .catch((err) => {
+            console.log(err)
+            debugger;
+        });
+};
+
+const postNewSignup = (newSignup) => {
+    axios
+        .post("https://reqres.in/api/users", newSignup)
+        .then ((res) => {
+            setSignupErrors([res.data, ...signup]);
+            setSignupValues(initialSUValues);
+        })
+}
+
+
+
+
+const onSubmit = (evt) => {
+    evt.preventDefault();
+}
+
+
+return (
+    <div>
+
+        <h1>Secret Family Recipes</h1>
+
         <div className="signUpPage">
-            <form>
+            <form onSubmit={onSubmit}>
                 <label>
                     <input
                     id="SUname"
@@ -41,7 +106,13 @@ export default function SignUpPage() {
                     maxLength= "30"
                 />
                 </label>
+                <button type="submit" > Submit </button>
+                <div>
+                    <p>Already have an account?</p> 
+                    <Link to="/login">Login</Link>
+                </div>
             </form>
         </div>
+    </div>
     )
 }
