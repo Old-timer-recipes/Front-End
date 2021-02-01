97e1caa2d0e2e9ccb67227fc292bd2be7a494678
juancaruizc <juancruizcruz@gmail.com>
2021-01-31 21:45:53 -0700

    test

--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -1,12 +1,11 @@
-import axios from 'axios'
+import axios from 'axios';
 
 export const axiosWithAuth = () => {
-    const token = localStorage.getItem('token')
-    
-    return axios.create({
-        baseURL: "https://secretfamily-recipes.herokuapp.com/api",
-        headers: {
-            Authorization: token
-        }
-    })
-}
+  const token = localStorage.getItem('token');
+  return axios.create({
+    baseURL: 'https://secretfamily-recipes.herokuapp.com/api',
+    headers: {
+      Authorization: token,
+    },
+  });
+};
